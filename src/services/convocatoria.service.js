b766df3d83a28c917098a5ac4e0f7bc7a8e49776
abraham <huancaaruquipaabraham@gmail.com>
2024-10-23 11:24:34 -0400

    23/10/2024

--- src/services/convocatoria.service.js
+++ src/services/convocatoria.service.js
@@ -2,7 +2,7 @@ import { Api } from "./api.service"
 
 export default {
     actulizarColumna(datos){
-        return Api().post("/api/convocatoria/actualizar", datos)
+        return Api().patch("/api/convocatoria/actualizar", datos)
     },
     obtenerColumna(datos){
         return Api().post("/api/convocatoria/obtener", datos)
